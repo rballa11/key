ad86a2c6300e22dbcb6bff1ddb3fffd39e48f803
Rohan Balla <rohanballa7@gmail.com>
2023-09-12 23:01:05 -0500

    fixed rel

--- src/components/EmbeddedLink.jsx
+++ src/components/EmbeddedLink.jsx
@@ -5,7 +5,7 @@ const EmbeddedLink = ({link, text}) => {
     //const [texts, setText] = useState(text);
     return (
         <div>
-            <a href = {link} target = "_blank"> {text} </a>
+            <a href = {link} target = "_blank" rel="noreferrer"> {text} </a>
         </div>
     );
     //need to figure out how to define values for link and text
