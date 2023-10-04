182599a4d891863a82a5a47903126004d8668a16
Rohan Balla <rohanballa7@gmail.com>
2023-10-03 22:37:53 -0500

    Worked on finishing home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,23 @@ import React from "react";
 
 import EmbeddedLink from "../components/EmbeddedLink";
 import Event from "../components/Event"
-
+import Logo from "../pics/logo.png"
 import "./home.css"
 
 
 const Home = () => {
     return (
         <div>
+            
         <h1>ORHSKeyClub</h1>
-        <link rel="icon" href= "../pics/logo.png" />
+        <EmbeddedLink link = "/leader" text = "Leadership"/>
+        <img src = {Logo} alt = "ORHS Key Club Logo" width = "200" height = "200" />    
+        <h2>Latest News - Remind : @orhskey24</h2>
+        
+        <h3>Check Slides for Upcoming Volunteering Events!</h3>
+        <EmbeddedLink link = "https://docs.google.com/presentation/d/13MrcRFpNese-70itY4Bl29tCwd72PbpxKCUvrs-_rgs/edit?usp=sharing" text = "Google Slides End of September Meeting"/>
         <EmbeddedLink link = "https://docs.google.com/presentation/d/1s_9DIuxsa_KFVteJbhzUkfWnePULghNnzRpibEv6IEE/edit?usp=sharing" text = "Google Slides Early September Meeting"/>
         <EmbeddedLink link = "https://www.instagram.com/keycluborhs/" text = "Insta"/>
-        <Event style = {{marginLeft: ".5em"}} title = "bruh" description = "mid" link = "https://getbootstrap.com/docs/4.0/content/typography/" date = "March 5th, 2023" />
-        
         </div>
     );
 }
--- src/pages/Leader.jsx
+++ src/pages/Leader.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import EmbeddedLink from "../components/EmbeddedLink";
+import Leadership from "../pics/leadership.png"
 
 const Leader = () => {
     return(
         <div>
-            <h1>Leader</h1>
+            <h1>Leadership of Key Club</h1>
+            <EmbeddedLink link = "/" text = "Home"/>
+            <img src = {Leadership} alt = "ORHS Leadership" width = "1000" height = "500" /> 
         </div>
     );
 }
